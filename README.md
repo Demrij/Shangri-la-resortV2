5e7cf9822b04e6941522bb423e668760266a556f
demri <johnsondemri@gmail.com>
2025-04-15 21:29:49 -0600

    gave final attempt at making weather api work- starting attributions in readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Shangri-la-resortV2
 could not commit on last repository, i exceeded the file size limit and couldn't push or commit anything and i was done figting with it, so im leaving out all large media and starrting this new repository.
+
+
+Attributions:
+
+All content 
--- weather.java
+++ weather.java
@@ -1,21 +1,27 @@
 function getWeather(){
 
     const apiKey = '68b0c54f38bc04e685856e5311c63301';
+
+    const lat = 21.1619;
+    const lon = -86.8518;
     
-    const url = 'https://api.openweathermap.org/data/2.5/weather?q=Cancunappid=${apiKey}&units=metric'
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
 
     fetch(url) 
         .then(response => response.json())
         .then(data => {
+
             const temp = Math.round(data.main.temp);
-            document.getElementById('.weather').textContent = `${temp}°C`;
 
-        })
+            document.querySelector(".weather").textContext = `${temp}°C`;
 
-        .catch(error => {
-            document.getElementById('.weather').textContent = 'N/A';
-        });
-        
+});
+
+.catch(error => {
+
+    document.querySelector(".weather").textContext = "N/A";
+})
 
 }
+
 getWeather();
